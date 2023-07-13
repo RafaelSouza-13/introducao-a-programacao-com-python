ac31d075cc327172ed6694a86baf98d7c9c5332b
Rafael Souza <rafa.nds13@gmail.com>
2023-07-12 21:46:16 -0300

    Melhorando a questao de lista com recursividade

--- capitulo-8/exe-8.16.py
+++ capitulo-8/exe-8.16.py
@@ -1,14 +1,14 @@
 def imprimir_lista(lista, nivel=0):
   espacamento = "\t" * nivel
-  print(espacamento, end="")
   for elemento in lista:
     if type(elemento) is int:
-      print(f'{elemento}', end=" ")
+      print(espacamento, end="")
+      print(f'{elemento}')
       continue
     else:
-      print()
-      nivel += 1
-      imprimir_lista(elemento, nivel)
+      print(espacamento+'{')
+      imprimir_lista(elemento, nivel + 1)
+      print(espacamento+'}')
 
 l = [1, [2, 3, 4, [5, 6, 7]]]
 imprimir_lista(l)
